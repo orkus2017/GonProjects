2fd7e04ed94c5cb82ab4758055dd32dfe4bdfb42
Fernando <fromero1974@hotmail.com>
2017-12-12 16:36:30 +0100

    Ok

---
+++ 2017_12_12_01_PruebaConGitHub/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_12_12_01_PruebaConGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PruebaDeFuncion("¡¡Hola mundo!!");
+
+            Console.ReadKey();
+        }
+
+        private static void PruebaDeFuncion(string paramString)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Llamada a PruebaDeFuncion");
+            Console.WriteLine("Parámetro enviado: '" + paramString + "'");
+            Console.WriteLine("");
+        }
+    }
+}
