803c525abeac7001c67544d09aace0b968bdf1f8
Fernando <fromero1974@hotmail.com>
2017-12-27 10:21:15 +0100

    Ok

---
+++ Patrones_003_Simple_Factory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones_003_Simple_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
