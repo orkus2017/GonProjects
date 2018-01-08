96f47040d3cf270a54b1e02487bed697c29aab69
Fernando <fromero1974@hotmail.com>
2018-01-08 01:41:48 +0100

    Ejemplos de la certicicacion 70-483

---
+++ 70-483 Chapter 1.1 ThreadMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace _70_483_Chapter_1._1_ThreadMethod
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);//PAsa el control a otros hilos
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            t.Join();//Espera a que termine el hilo t para proseguir
+
+            Console.ReadKey();
+        }
+    }
+}
